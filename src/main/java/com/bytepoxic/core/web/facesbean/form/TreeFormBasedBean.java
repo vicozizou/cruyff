698d1d6829463313c32ee109c6d8e9cb170d8f51
victor.alfaro <vico.zizou@gmail.com>
2013-03-04 17:37:14 -0600

    Restructure of projects

--- src/main/java/com/bytepoxic/core/web/facesbean/form/TreeFormBasedBean.java
+++
@@ -1,5 +0,0 @@
-package com.aureabox.webcore.web.facesbean.form;
-
-public interface TreeFormBasedBean extends FormBasedBean {
-	String PARENT_ID_PARAM = "pid";
-}
--- src/main/java/com/bytepoxic/core/web/facesbean/form/ViewMode.java
+++
@@ -1,5 +0,0 @@
-package com.aureabox.webcore.web.facesbean.form;
-
-public enum ViewMode {
-	READ_ONLY_MODE, NEW_MODE, EDIT_MODE;
-}
